613af74f78cf0cd9963d62d7ff0ab669133cddf4
shrikant nimhan <shni9045@colorado.edu>
2021-11-19 14:55:41 -0700

    Alignment issue solved

--- meta-custom/recipes-custom/custom/files/values.cpp
+++ meta-custom/recipes-custom/custom/files/values.cpp
@@ -6,16 +6,26 @@ Values::Values()
     QVBoxLayout *layout = new QVBoxLayout;
 
     QLabel *temperature_l = new QLabel(tr("Temperature (°C)"));
+
+
+    QFont t = temperature_l->font();
+    t.setPointSize(200);
+    t.setBold(true);
+    temperature_l->setFont(t);
+
+
+    temperature_l->setAlignment(Qt::AlignTop);
+
     
     temperature_v = new QLabel();
 
 
     QFont f = temperature_v->font();
-    f.setPointSize(20);
+    f.setPointSize(100);
     f.setBold(true);
     temperature_v->setFont(f);
     
-    temperature_v->setAlignment(Qt::AlignVCenter);
+    temperature_v->setAlignment(Qt::AlignVCenter | Qt::AlignHCenter);
     
 
     layout->addWidget(temperature_l);
